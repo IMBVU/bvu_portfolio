f4c02a3b71226319e87ba6a6336b355aedbc2abc
Bryan Vu <134100395+IMBVU@users.noreply.github.com>
2025-02-10 16:51:43 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -95,7 +95,7 @@ export function Hero() {
             >
               <div className="relative w-full aspect-square">
                 <img
-                  src={"/src/components/bvu_offical PI.png"}
+                  src={"/src/components/Profile_Image.JPG"}
                   alt="Profile"
                   className="w-full h-full object-cover rounded-full shadow-2xl"
                 />
